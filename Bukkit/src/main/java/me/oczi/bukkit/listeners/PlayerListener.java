8a88cc945a6493352851ef6d42940f09133b9461
OcZi <atlas200x@gmail.com>
2021-01-24 20:39:29 -0500

    Fix player teleport to their partner when right clicking on any player

--- Bukkit/src/main/java/me/oczi/bukkit/listeners/PlayerListener.java
+++ Bukkit/src/main/java/me/oczi/bukkit/listeners/PlayerListener.java
@@ -6,6 +6,7 @@ import me.oczi.bukkit.utils.MargaretPlayers;
 import me.oczi.bukkit.utils.PartnershipPermission;
 import me.oczi.bukkit.utils.Partnerships;
 import me.oczi.bukkit.utils.settings.PartnershipSetting;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -30,7 +31,10 @@ public class PlayerListener implements Listener {
 
   @EventHandler
   public void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
-    if (!(e.getRightClicked() instanceof Player)) { return; }
+    Entity entity = e.getRightClicked();
+    if (!(entity instanceof Player)) {
+      return;
+    }
 
     Player player1 = e.getPlayer();
     MargaretPlayer margaretPlayer1 = MargaretPlayers
@@ -39,14 +43,15 @@ public class PlayerListener implements Listener {
       return;
     }
 
-    MargaretPlayer margaretPlayer2 = Partnerships
-        .foundPartnerAsMargaretPlayer(margaretPlayer1);
-    if (margaretPlayer2.isEmpty()) {
+    Player player2 = (Player) entity;
+    UUID partnerUuid = Partnerships.foundUuidOfPartner(margaretPlayer1);
+    UUID uuid = player2.getUniqueId();
+    if (!uuid.equals(partnerUuid)) {
       return;
     }
+    MargaretPlayer margaretPlayer2 =
+        MargaretPlayers.getAsMargaretPlayer(uuid);
     if (margaretPlayer2.isSetting(PartnershipSetting.ALLOW_MOUNT)) {
-      Player player2 = MargaretPlayers
-          .getAsPlayer(margaretPlayer2);
       if (player1.getPassenger() == null) {
         player2.setPassenger(player1);
       }
@@ -55,17 +60,23 @@ public class PlayerListener implements Listener {
 
   @EventHandler
   public void onPvp(EntityDamageByEntityEvent e) {
-    if (!(e.getEntity() instanceof Player)) { return; }
+    if (!(e.getEntity() instanceof Player)) {
+      return;
+    }
     Player player1 = (Player) e.getEntity();
     MargaretPlayer margaretPlayer1 = MargaretPlayers
         .getAsMargaretPlayer(player1);
-    if (!margaretPlayer1.havePartner()) { return; }
+    if (!margaretPlayer1.havePartner()) {
+      return;
+    }
 
     Partnership partnership = margaretPlayer1.getPartnership();
     UUID partnerUuid = Partnerships.foundUuidOfPartner(margaretPlayer1);
     MargaretPlayer margaretPlayer2 = MargaretPlayers
         .getAsMargaretPlayer(partnerUuid);
-    if (margaretPlayer2.isEmpty()) { return; }
+    if (margaretPlayer2.isEmpty()) {
+      return;
+    }
     if (partnership.hasPermission(PartnershipPermission.PVP) &&
         !margaretPlayer2.isSetting(PartnershipSetting.ALLOW_PVP)) {
       e.setCancelled(true);
